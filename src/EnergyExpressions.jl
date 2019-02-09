a3ee2d8b7473a17e59f7197aedc5d8425110cd48
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-02-09 13:34:02 +0100

    Cleanout old implementation

--- src/EnergyExpressions.jl
+++ src/EnergyExpressions.jl
@@ -12,16 +12,12 @@ if VERSION < v"1.1-DEV"
 end
 
 include("conjugate_orbitals.jl")
-# include("one_body.jl")
-# include("repulsion_potentials.jl")
-# include("two_body.jl")
-# include("energy_expressions.jl")
-# include("invariant_sets.jl")
 include("slater_determinants.jl")
 include("nbody_operators.jl")
 include("nbody_matrix_elements.jl")
 include("common_operators.jl")
 include("equations.jl")
 include("variations.jl")
+# include("invariant_sets.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,3 @@ using AtomicLevels
 using Test
 
 include("conjugate_orbitals.jl")
-# include("one_body.jl")
-# include("repulsion_potentials.jl")
-# include("two_body.jl")
-# include("energy_expressions.jl")
