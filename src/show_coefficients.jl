60c9c3709e744366a1782adc0480d7749041b7b4
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2020-01-23 13:58:03 +0100

    Fixed display of real coefficients stored as complex type

--- src/show_coefficients.jl
+++ src/show_coefficients.jl
@@ -2,7 +2,7 @@ function showcoeff(io::IO, n::Number, show_sign::Bool, show_one::Bool=false)
     isimag(n) = !iszero(n) && isreal(im*n)
     isneg(n) = n < 0
 
-    if isreal(n) && isneg(n) || isimag(n) && isneg(imag(n))
+    if isreal(n) && isneg(real(n)) || isimag(n) && isneg(imag(n))
         write(io, "- ")
     else
         show_sign && write(io, "+ ")
